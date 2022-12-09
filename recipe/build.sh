6a444ca8d3c130f76839af778ce38c0cb8b6952e
Uwe L. Korn <uwe.korn@quantco.com>
2022-12-09 17:42:45 +0100

    Explicitly set C standard

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export CFLAGS="${CFLAGS} -std=c99"
+
 make all
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check test-xxhsum-c namespaceTest
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
+  make check test-xxhsum-c namespaceTest
 fi
 make install
